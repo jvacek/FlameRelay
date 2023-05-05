1abcf4586965b5cc4940f287df32d07313aacb9b
Jonas Vacek <jvacek@users.noreply.github.com>
2023-05-06 00:26:02 +0200

    Add checkin creation page

--- flamerelay/backend/urls.py
+++ flamerelay/backend/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from flamerelay.backend.views import unit_view
+from flamerelay.backend.views import checkin_create_view, unit_view
 
 app_name = "backend"
 urlpatterns = [
     path("unit/<str:identifier>/", view=unit_view, name="unit"),
+    path("unit/<str:identifier>/checkin", view=checkin_create_view, name="checkin"),
 ]
--- flamerelay/backend/views.py
+++ flamerelay/backend/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import get_object_or_404, render
+from django.contrib.auth.decorators import login_required
+from django.forms import ModelForm
+from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 
-from .models import Unit
+from .models import CheckIn, Unit
 
 
 # View for the unit page
@@ -8,3 +11,37 @@ def unit_view(request, identifier):
     unit = get_object_or_404(Unit, identifier=identifier)
     map_html = unit.get_map()._repr_html_()
     return render(request, "backend/unit.html", {"unit": unit, "map_html": map_html})
+
+
+# A view for the CheckIn object creation using a form
+@login_required
+def checkin_create_view(request, identifier):
+    class CheckInForm(ModelForm):
+        class Meta:
+            model = CheckIn
+            fields = [
+                # "unit",
+                # "date_created",
+                # "created_by",
+                "image",
+                "message",
+                "city",
+                "location",
+            ]
+
+    form = CheckInForm(request.POST or None, initial={"unit": identifier})
+    if form.is_valid():
+        print(form.cleaned_data)
+        form.unit = Unit.objects.filter(identifier=identifier)
+        form.created_by = request.user
+        CheckIn.objects.create(
+            created_by=request.user,
+            unit=Unit.objects.get(identifier=identifier),
+            **form.cleaned_data,
+        )
+        # form.save()
+        # form = CheckInForm()
+        return redirect(reverse("backend:unit", kwargs={"identifier": identifier}))
+
+    context = {"form": form}
+    return render(request, "backend/checkin_create.html", context)
