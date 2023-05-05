1eb81e6c6bfc262e1dc946235a004a338cae1af2
Jonas Vacek <jvacek@users.noreply.github.com>
2023-05-06 00:23:46 +0200

    Add checkin inline to admin

--- flamerelay/backend/admin.py
+++ flamerelay/backend/admin.py
@@ -3,11 +3,17 @@ from django.contrib import admin
 from .models import CheckIn, Unit
 
 
+class CheckInInline(admin.TabularInline):
+    model = CheckIn
+    extra = 0
+
+
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
     list_display = ("id", "identifier", "date_created", "created_by")
     list_filter = ("date_created", "created_by")
     filter_horizontal = ["subscribers"]
+    inlines = [CheckInInline]
 
 
 @admin.register(CheckIn)
