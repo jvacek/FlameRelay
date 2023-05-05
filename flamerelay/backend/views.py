cd2513c59dfa736fbc59d800e7ea1061e8784dd6
Jonas Vacek <jvacek@users.noreply.github.com>
2023-05-05 23:26:21 +0200

    Fix the view

--- flamerelay/backend/views.py
+++ flamerelay/backend/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render  # get_object_or_404
+from django.shortcuts import get_object_or_404, render
 
-# from .services import create_map
-# from .models import Unit
+from .models import Unit
 
 
 # View for the unit page
 def unit_view(request, identifier):
-    # unit = get_object_or_404(Unit, identifier=identifier)
-    # map_html = create_map(unit.checkin_set.order('date_created'))
-    return render(request, "backend/unit.html")  # , {'unit': unit, 'map_html': map_html})
+    unit = get_object_or_404(Unit, identifier=identifier)
+    map_html = unit.get_map()._repr_html_()
+    return render(request, "backend/unit.html", {"unit": unit, "map_html": map_html})
