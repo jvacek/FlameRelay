734075c9f367f22f39545ac05de8d4c757b59d17
Jonas Vacek <jvacek@users.noreply.github.com>
2023-05-16 02:33:00 +0200

    Fix update script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-pushd ~/Git/flamerelay
+pushd ~/Git/FlameRelay
 git fetch
 git reset --hard origin/master
 docker-compose -f production.yml build
 docker-compose -f production.yml up --force-recreate -d
+docker system prune -a -f
+popd
