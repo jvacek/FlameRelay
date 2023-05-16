e0546a18bd88f7ad97f4980dbaf12703675c1b55
Jonas Vacek <jvacek@users.noreply.github.com>
2023-05-16 02:42:14 +0200

    use right branch lol

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pushd ~/Git/FlameRelay
 git fetch
-git reset --hard origin/master
+git reset --hard origin/main
 docker-compose -f production.yml build
 docker-compose -f production.yml up --force-recreate -d
 docker system prune -a -f
