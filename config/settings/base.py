8220ab933115d5d30a61d7d1380b7630dcc0d554
Jonas Vacek <jvacek@users.noreply.github.com>
2023-05-22 12:52:46 +0200

    config for facebook login

--- config/settings/base.py
+++ config/settings/base.py
@@ -347,3 +347,29 @@ DJANGORESIZED_DEFAULT_KEEP_META = False
 DJANGORESIZED_DEFAULT_FORCE_FORMAT = "JPEG"
 DJANGORESIZED_DEFAULT_FORMAT_EXTENSIONS = {"JPEG": ".jpg"}
 DJANGORESIZED_DEFAULT_NORMALIZE_ROTATION = True
+
+
+SOCIALACCOUNT_PROVIDERS = {
+    "facebook": {
+        "METHOD": "oauth2",
+        "SDK_URL": "//connect.facebook.net/{locale}/sdk.js",
+        "SCOPE": ["email", "public_profile"],
+        "AUTH_PARAMS": {"auth_type": "reauthenticate"},
+        "INIT_PARAMS": {"cookie": True},
+        "FIELDS": [
+            # 'id',
+            # 'first_name',
+            # 'last_name',
+            # 'middle_name',
+            "name",
+            # 'name_format',
+            # 'picture',
+            # 'short_name'
+        ],
+        "EXCHANGE_TOKEN": True,
+        # "LOCALE_FUNC": "path.to.callable",
+        "VERIFIED_EMAIL": False,
+        "VERSION": "v13.0",
+        "GRAPH_API_URL": "https://graph.facebook.com/v13.0",
+    }
+}
