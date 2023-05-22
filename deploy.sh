377033ccd7481528d14ac56a6d3db7987e84c69a
Jonas Vacek <jvacek@users.noreply.github.com>
2023-05-22 13:12:40 +0200

    graceful shutdown first

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ pushd ~/Git/FlameRelay
 git fetch
 git reset --hard origin/main
 docker-compose -f production.yml build
-docker-compose -f production.yml up --force-recreate -d
+docker-compose -f production.yml down -t 20
+docker-compose -f production.yml up -d
 # docker system prune -a -f
 docker system prune -f
 popd
