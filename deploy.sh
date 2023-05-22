e375b5141d78191e4048b7d35b2ace247eb11ffc
Jonas Vacek <jvacek@users.noreply.github.com>
2023-05-22 13:14:42 +0200

    stop pruning

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ docker-compose -f production.yml build
 docker-compose -f production.yml down -t 20
 docker-compose -f production.yml up -d
 # docker system prune -a -f
-docker system prune -f
+# docker system prune -f
 popd
