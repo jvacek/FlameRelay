5e72147e9adc6b007119930bedfce5a524d35acb
Jonas Vacek <1302278+jvacek@users.noreply.github.com>
2026-04-19 17:06:50 +0200

    Tweaks on email definition

--- config/settings/production.py
+++ config/settings/production.py
@@ -93,14 +93,14 @@ STORAGES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="hello@litroute.com",
+    default="LitRoute <hello@litroute.com>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
 # https://docs.djangoproject.com/en/dev/ref/settings/#email-subject-prefix
 EMAIL_SUBJECT_PREFIX = env(
     "DJANGO_EMAIL_SUBJECT_PREFIX",
-    default="[LitRoute]",
+    default="",
 )
 ACCOUNT_EMAIL_SUBJECT_PREFIX = EMAIL_SUBJECT_PREFIX
 
