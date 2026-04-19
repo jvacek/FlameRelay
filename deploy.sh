29bc937b0cedf3e4c149f8ded5fdd36ae2a0deef
Jonas Vacek <jvacek@ota-jonasvacek-d0xn2.hyrax-chicken.ts.net>
2026-04-19 14:16:34 +0200

    build in CI to ghcr.io

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,8 @@
 pushd ~/Git/FlameRelay
 git fetch
 git reset --hard origin/main
-docker compose -f docker-compose.production.yml build
+docker compose -f docker-compose.production.yml pull
 docker compose -f docker-compose.production.yml down -t 20
 docker compose -f docker-compose.production.yml up -d
-# docker system prune -a -f
-# docker system prune -f
+docker image prune -f
 popd
