6a866d78adb1a9f7e5bce9032aba55e99aa1f5d3
Jonas Vacek <1302278+jvacek@users.noreply.github.com>
2026-04-21 00:22:13 +0200

    decrease task expiry in celery

--- config/settings/base.py
+++ config/settings/base.py
@@ -351,6 +351,8 @@ CELERY_WORKER_SEND_TASK_EVENTS = True
 CELERY_TASK_SEND_SENT_EVENT = True
 # https://docs.celeryq.dev/en/stable/userguide/configuration.html#worker-hijack-root-logger
 CELERY_WORKER_HIJACK_ROOT_LOGGER = False
+# https://docs.celeryq.dev/en/stable/userguide/configuration.html#result-expires
+CELERY_RESULT_EXPIRES = 3600
 # django-allauth
 # ------------------------------------------------------------------------------
 ACCOUNT_ALLOW_REGISTRATION = env.bool("DJANGO_ACCOUNT_ALLOW_REGISTRATION", True)
