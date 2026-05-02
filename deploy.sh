6a83600736f18697f397d151fb92def20502198e
Jonas Vacek <jvacek@users.noreply.github.com>
2026-05-02 21:01:21 +0200

    fix domain in brand guidelines?

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 pushd ~/Git/FlameRelay
 git fetch
 git reset --hard origin/main
+mkdir -p /srv/flamerelay/media
+chown -R 100:101 /srv/flamerelay/media
 docker compose -f docker-compose.production.yml pull
 docker compose -f docker-compose.production.yml down -t 20
 docker compose -f docker-compose.production.yml up -d
