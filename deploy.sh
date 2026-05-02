876f2d00fa7eaae64816bb5e2585ccf9a0a2f045
Jonas Vacek <jvacek@users.noreply.github.com>
2026-05-02 21:02:34 +0200

    change UIDs for docker for fs perms and avoiding collisions with other system users

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ pushd ~/Git/FlameRelay
 git fetch
 git reset --hard origin/main
 mkdir -p /srv/flamerelay/media
-chown -R 100:101 /srv/flamerelay/media
+chown -R 10001:10001 /srv/flamerelay/media
 docker compose -f docker-compose.production.yml pull
 docker compose -f docker-compose.production.yml down -t 20
 docker compose -f docker-compose.production.yml up -d
