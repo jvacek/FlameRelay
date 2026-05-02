7a5c67af98f38954a47a6f62fd178f3e91e560a9
Jonas Vacek <jvacek@users.noreply.github.com>
2026-05-02 16:43:17 +0200

    move migration

--- backend/migrations/0015_game_unit_game.py
+++ backend/migrations/0015_game_unit_game.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("backend", "0013_alter_checkin_unit"),
+        ("backend", "0014_contributor_group"),
     ]
 
     operations = [
