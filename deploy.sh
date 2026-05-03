96a22e432aeaf620eec5548ed14a884fb6dd7ff7
Jonas Vacek <jvacek@users.noreply.github.com>
2026-05-03 02:32:01 +0200

    comment out deploy commands that need sudo

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 pushd ~/Git/FlameRelay
 git fetch
 git reset --hard origin/main
-mkdir -p /srv/flamerelay/media
-chown -R 10001:10001 /srv/flamerelay/media
+# mkdir -p /srv/flamerelay/media
+# chown -R 10001:10001 /srv/flamerelay/media
 docker compose -f docker-compose.production.yml pull
 docker compose -f docker-compose.production.yml down -t 20
 docker compose -f docker-compose.production.yml up -d
