065a68adc9fc1ec3575df667300fef53ef0331fe
Jonas Vacek <jvacek@users.noreply.github.com>
2026-05-06 20:56:12 +0200

    Fix CSRF cookie missing for anonymous users on guest email subscribe (#89)

--- flamerelay/users/views.py
+++ flamerelay/users/views.py
@@ -1,3 +1,4 @@
+from django.views.decorators.csrf import ensure_csrf_cookie
 from django.views.generic import TemplateView
 
-spa_view = TemplateView.as_view(template_name="spa.html")
+spa_view = ensure_csrf_cookie(TemplateView.as_view(template_name="spa.html"))
